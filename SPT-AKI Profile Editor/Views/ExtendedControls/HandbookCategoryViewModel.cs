e052738a67026e7e12dff68ebd239955f1756bf1
Дмитрий Худяков <skit.92@mail.ru>
2022-01-09 20:29:24 +0600

    Added HandbookCategoryViewModel

---
+++ SPT-AKI Profile Editor/Views/ExtendedControls/HandbookCategoryViewModel.cs
@@ -0,0 +1,24 @@
+﻿using SPT_AKI_Profile_Editor.Core.ServerClasses;
+using SPT_AKI_Profile_Editor.Helpers;
+
+namespace SPT_AKI_Profile_Editor.Views.ExtendedControls
+{
+    class HandbookCategoryViewModel : BindableViewModel
+    {
+        public HandbookCategoryViewModel(HandbookCategory category)
+        {
+            Category = category;
+        }
+        public HandbookCategory Category
+        {
+            get => category;
+            set
+            {
+                category = value;
+                OnPropertyChanged("ItemCategory");
+            }
+        }
+
+        private HandbookCategory category;
+    }
+}
