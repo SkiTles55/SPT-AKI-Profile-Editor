c1427a06fe0b86cfa2a2da8778a07dc94ad70b40
Dmitry Khudyakov <skit.92@mail.com>
2022-05-07 23:04:41 +0600

    WeaponBuild refactoring

--- SPT-AKI Profile Editor.Tests/TestConstants.cs
+++ SPT-AKI Profile Editor.Tests/TestConstants.cs
@@ -5,7 +5,7 @@ namespace SPT_AKI_Profile_Editor.Tests
 {
     internal class TestConstants
     {
-        public static readonly string profileFile = @"C:\SPT\user\profiles\99ccaab4eee294d7f3b432af.json";
+        public static readonly string profileFile = @"C:\SPT\user\profiles\3aabedcdece6b7093c76d410.json";
 
         public static readonly string serverPath = @"C:\SPT";
 
--- SPT-AKI Profile Editor/Core/ProfileClasses/WeaponBuild.cs
+++ SPT-AKI Profile Editor/Core/ProfileClasses/WeaponBuild.cs
@@ -7,14 +7,19 @@ namespace SPT_AKI_Profile_Editor.Core.ProfileClasses
 {
     public class WeaponBuild
     {
-        [JsonIgnore]
-        private float ergonomics = 0;
+        private List<InventoryItem> BuildItems = new();
 
-        [JsonIgnore]
-        private int recoilForceUp = 0;
+        private float RecoilDelta = 0;
 
-        [JsonIgnore]
-        private int recoilForceBack = 0;
+        [JsonConstructor]
+        public WeaponBuild(string id, string name, string root, object[] items)
+        {
+            Id = id;
+            Name = name;
+            Root = root;
+            Items = items;
+            CalculateBuildProperties();
+        }
 
         [JsonProperty("id")]
         public string Id { get; set; }
@@ -29,63 +34,60 @@ namespace SPT_AKI_Profile_Editor.Core.ProfileClasses
         public object[] Items { get; set; }
 
         [JsonIgnore]
-        public string Weapon =>
-            BuildItems
-            .Where(x => x.Id == Root)
-            .FirstOrDefault()
-            .LocalizedName;
+        public string Weapon { get; set; }
 
         [JsonIgnore]
-        public float Ergonomics => ergonomics;
+        public float Ergonomics { get; set; }
 
         [JsonIgnore]
-        public int RecoilForceUp => recoilForceUp;
+        public int RecoilForceUp { get; set; }
 
         [JsonIgnore]
-        public int RecoilForceBack => recoilForceBack;
+        public int RecoilForceBack { get; set; }
 
         [JsonIgnore]
-        public bool HasModdedItems =>
-            BuildItems
-            .Any(x => !AppData.ServerDatabase.ItemsDB.ContainsKey(x.Tpl));
+        public bool HasModdedItems { get; set; }
 
-        [JsonIgnore]
-        private List<InventoryItem> BuildItems
+        private void CalculateBuildProperties()
         {
-            get
+            List<InventoryItem> buildItems = new();
+            foreach (var obj in Items)
             {
-                List<InventoryItem> items = new();
-                float RecoilDelta = 0;
-                foreach (var obj in Items)
+                try
                 {
-                    try
-                    {
-                        var item = JsonConvert.DeserializeObject<InventoryItem>(obj.ToString());
-                        items.Add(item);
-                        if (item.Id == Root)
-                        {
-                            if (AppData.ServerDatabase.ItemsDB.ContainsKey(item.Tpl) && AppData.ServerDatabase.ItemsDB[item.Tpl].Properties != null)
-                            {
-                                recoilForceUp = AppData.ServerDatabase.ItemsDB[item.Tpl].Properties.RecoilForceUp;
-                                recoilForceBack = AppData.ServerDatabase.ItemsDB[item.Tpl].Properties.RecoilForceBack;
-                                ergonomics = AppData.ServerDatabase.ItemsDB[item.Tpl].Properties.Ergonomics;
-                            }
-                        }
-                        else
-                        {
-                            if (AppData.ServerDatabase.ItemsDB.ContainsKey(item.Tpl) && AppData.ServerDatabase.ItemsDB[item.Tpl].Properties != null)
-                            {
-                                RecoilDelta += AppData.ServerDatabase.ItemsDB[item.Tpl].Properties.Recoil;
-                                ergonomics += AppData.ServerDatabase.ItemsDB[item.Tpl].Properties.Ergonomics;
-                            }
-                        }
-                    }
-                    catch (Exception ex) { Logger.Log($"WeaponBuilds weapon item loading error: {ex.Message}"); }
+                    var item = JsonConvert.DeserializeObject<InventoryItem>(obj.ToString());
+                    buildItems.Add(item);
+                    if (item.Id == Root)
+                        SetupWeaponProperties(item);
+                    else
+                        AddModProperties(item);
                 }
-                RecoilDelta /= 100f;
-                recoilForceUp = (int)Math.Round(recoilForceUp + recoilForceUp * RecoilDelta);
-                recoilForceBack = (int)Math.Round(recoilForceBack + recoilForceBack * RecoilDelta);
-                return items;
+                catch (Exception ex) { Logger.Log($"WeaponBuilds weapon item loading error: {ex.Message}"); }
+            }
+            RecoilDelta /= 100f;
+            RecoilForceUp = (int)Math.Round(RecoilForceUp + RecoilForceUp * RecoilDelta);
+            RecoilForceBack = (int)Math.Round(RecoilForceBack + RecoilForceBack * RecoilDelta);
+            BuildItems = buildItems;
+            HasModdedItems = buildItems.Any(x => !AppData.ServerDatabase.ItemsDB.ContainsKey(x.Tpl));
+            Weapon = buildItems.Where(x => x.Id == Root).FirstOrDefault().LocalizedName;
+        }
+
+        private void AddModProperties(InventoryItem item)
+        {
+            if (AppData.ServerDatabase.ItemsDB.ContainsKey(item.Tpl) && AppData.ServerDatabase.ItemsDB[item.Tpl].Properties != null)
+            {
+                RecoilDelta += AppData.ServerDatabase.ItemsDB[item.Tpl].Properties.Recoil;
+                Ergonomics += AppData.ServerDatabase.ItemsDB[item.Tpl].Properties.Ergonomics;
+            }
+        }
+
+        private void SetupWeaponProperties(InventoryItem item)
+        {
+            if (AppData.ServerDatabase.ItemsDB.ContainsKey(item.Tpl) && AppData.ServerDatabase.ItemsDB[item.Tpl].Properties != null)
+            {
+                RecoilForceUp = AppData.ServerDatabase.ItemsDB[item.Tpl].Properties.RecoilForceUp;
+                RecoilForceBack = AppData.ServerDatabase.ItemsDB[item.Tpl].Properties.RecoilForceBack;
+                Ergonomics = AppData.ServerDatabase.ItemsDB[item.Tpl].Properties.Ergonomics;
             }
         }
     }
