0af67de5acef2326447b8410d10bb7e3154c555f
Dmitry Khudyakov <skit.92@mail.com>
2022-05-08 23:12:18 +0600

    Inventory as separate UserControl

---
+++ SPT-AKI Profile Editor/Views/ExtendedControls/Inventory.xaml.cs
@@ -0,0 +1,43 @@
+﻿using SPT_AKI_Profile_Editor.Core;
+using SPT_AKI_Profile_Editor.Core.ProfileClasses;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace SPT_AKI_Profile_Editor.Views.ExtendedControls
+{
+    /// <summary>
+    /// Логика взаимодействия для Inventory.xaml
+    /// </summary>
+    public partial class Inventory : UserControl
+    {
+        public Inventory()
+        {
+            InitializeComponent();
+        }
+
+        private void FilterBoxStash_TextChanged(object sender, TextChangedEventArgs e) =>
+            ApplyStashFilter();
+
+        private void ApplyStashFilter()
+        {
+            ICollectionView cv = CollectionViewSource.GetDefaultView(stashGrid.ItemsSource);
+            if (cv == null)
+                return;
+            if (string.IsNullOrEmpty(AppData.GridFilters.StashTab.StashItemName)
+                && string.IsNullOrEmpty(AppData.GridFilters.StashTab.Id))
+                cv.Filter = null;
+            else
+            {
+                cv.Filter = o =>
+                {
+                    InventoryItem p = o as InventoryItem;
+                    return (string.IsNullOrEmpty(AppData.GridFilters.StashTab.StashItemName)
+                    || p.LocalizedName.ToUpper().Contains(AppData.GridFilters.StashTab.StashItemName.ToUpper()))
+                    && (string.IsNullOrEmpty(AppData.GridFilters.StashTab.Id)
+                    || p.Id.ToUpper().Contains(AppData.GridFilters.StashTab.Id.ToUpper()));
+                };
+            }
+        }
+    }
+}
