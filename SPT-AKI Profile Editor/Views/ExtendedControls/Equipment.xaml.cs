ba5146fb0c8f567285e21e4143a77d21fcb2b7e6
Dmitry Khudyakov <skit.92@mail.com>
2022-05-11 23:38:39 +0600

    Start creating Equipment tabs

---
+++ SPT-AKI Profile Editor/Views/ExtendedControls/Equipment.xaml.cs
@@ -0,0 +1,26 @@
+﻿using SPT_AKI_Profile_Editor.Core.ProfileClasses;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SPT_AKI_Profile_Editor.Views.ExtendedControls
+{
+    /// <summary>
+    /// Логика взаимодействия для Equipment.xaml
+    /// </summary>
+    public partial class Equipment : UserControl
+    {
+        public static readonly DependencyProperty InventoryEquipmentProperty =
+            DependencyProperty.Register(nameof(InventoryEquipment), typeof(CharacterInventory), typeof(Equipment), new PropertyMetadata(null));
+
+        public Equipment()
+        {
+            InitializeComponent();
+        }
+
+        public CharacterInventory InventoryEquipment
+        {
+            get { return (CharacterInventory)GetValue(InventoryEquipmentProperty); }
+            set { SetValue(InventoryEquipmentProperty, value); }
+        }
+    }
+}
