54bba13aa0ed81c44d868163e0dfe103f7ca7f01
Dmitry Khudyakov <skit.92@mail.com>
2022-06-04 20:47:27 +0600

    Added WeaponBuildsList

--- SPT-AKI Profile Editor/Views/WeaponBuilds.xaml.cs
+++ SPT-AKI Profile Editor/Views/WeaponBuilds.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace SPT_AKI_Profile_Editor.Views
 {
@@ -12,18 +11,6 @@ namespace SPT_AKI_Profile_Editor.Views
         {
             InitializeComponent();
             DataContext = new WeaponBuildsViewModel();
-        }
-
-        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            if (sender is ScrollViewer scrollViewer)
-            {
-                if (e.Delta > 0)
-                    scrollViewer.LineUp();
-                else
-                    scrollViewer.LineDown();
-                e.Handled = true;
-            }
         }
     }
 }
