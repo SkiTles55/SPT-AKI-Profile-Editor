785f73292f25b1734ccd4157ca164e73d4ca5b9c
Dmitry Khudyakov <skit.92@mail.com>
2022-06-04 12:17:40 +0600

    WeaponBuildsViewModel refactoring

--- SPT-AKI Profile Editor/Views/WeaponBuildsViewModel.cs
+++ SPT-AKI Profile Editor/Views/WeaponBuildsViewModel.cs
@@ -4,91 +4,94 @@ using SPT_AKI_Profile_Editor.Core.ProfileClasses;
 using SPT_AKI_Profile_Editor.Helpers;
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SPT_AKI_Profile_Editor.Views
 {
     internal class WeaponBuildsViewModel : BindableViewModel
     {
-        public static RelayCommand ExportBuild => new(obj =>
-          {
-              if (obj == null)
-                  return;
-              if (obj is not WeaponBuild build)
-                  return;
-              SaveFileDialog saveFileDialog = new()
-              {
-                  Filter = "Файл JSON (*.json)|*.json|All files (*.*)|*.*",
-                  FileName = $"Weapon preset {build.Name}",
-                  RestoreDirectory = true
-              };
-              if (saveFileDialog.ShowDialog() == DialogResult.OK)
-              {
-                  App.Worker.AddAction(new WorkerTask
-                  {
-                      Action = () => { Profile.ExportBuild(build, saveFileDialog.FileName); },
-                      Title = AppLocalization.GetLocalizedString("progress_dialog_title"),
-                      Description = AppLocalization.GetLocalizedString("tab_presets_export")
-                  });
-              }
-          });
+        public static RelayCommand ExportBuild => new(obj => ExportBuildToFile(obj));
 
-        public static RelayCommand ExportBuilds => new(obj =>
-          {
-              FolderBrowserDialog folderBrowserDialog = new()
-              {
-                  RootFolder = Environment.SpecialFolder.MyComputer,
-                  ShowNewFolderButton = true
-              };
-              if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
-              {
-                  foreach (var build in Profile.WeaponBuilds)
-                  {
-                      App.Worker.AddAction(new WorkerTask
-                      {
-                          Action = () => { Profile.ExportBuild(build.Value, Path.Combine(folderBrowserDialog.SelectedPath, $"Weapon preset {build.Value.Name}.json")); },
-                          Title = AppLocalization.GetLocalizedString("progress_dialog_title"),
-                          Description = AppLocalization.GetLocalizedString("tab_presets_export")
-                      });
-                  }
-              }
-          });
+        public static RelayCommand ExportBuilds => new(obj => ExportAllBuilds());
 
-        public static RelayCommand ImportBuilds => new(obj =>
-          {
-              OpenFileDialog openFileDialog = new()
-              {
-                  Filter = "Файл JSON (*.json)|*.json|All files (*.*)|*.*",
-                  RestoreDirectory = true,
-                  Multiselect = true
-              };
+        public static RelayCommand ImportBuilds => new(obj => ImportBuildsFromFiles());
 
-              if (openFileDialog.ShowDialog() == DialogResult.OK)
-              {
-                  foreach (var file in openFileDialog.FileNames)
-                  {
-                      App.Worker.AddAction(new WorkerTask
-                      {
-                          Action = () => { Profile.ImportBuildFromFile(file); },
-                          Title = AppLocalization.GetLocalizedString("progress_dialog_title"),
-                          Description = AppLocalization.GetLocalizedString("tab_presets_import")
-                      });
-                  }
-              }
-          });
+        public RelayCommand RemoveBuild => new(async obj => await RemoveBuildFromProfile(obj));
 
-        public RelayCommand RemoveBuild => new(async obj =>
-                                 {
-                                     if (obj == null)
-                                         return;
-                                     if (await Dialogs.YesNoDialog(this, "remove_preset_dialog_title", "remove_preset_dialog_caption"))
-                                         Profile.RemoveBuild(obj.ToString());
-                                 });
+        public RelayCommand RemoveBuilds => new(async obj => await RemoveAllBuildsFromProfile());
 
-        public RelayCommand RemoveBuilds => new(async obj =>
-         {
-             if (await Dialogs.YesNoDialog(this, "remove_preset_dialog_title", "remove_presets_dialog_caption"))
-                 Profile.RemoveBuilds();
-         });
+        private static void ExportBuildToFile(object obj)
+        {
+            if (obj == null || obj is not WeaponBuild build)
+                return;
+            SaveFileDialog saveFileDialog = new()
+            {
+                Filter = "Файл JSON (*.json)|*.json|All files (*.*)|*.*",
+                FileName = $"Weapon preset {build.Name}",
+                RestoreDirectory = true
+            };
+            if (saveFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+            App.Worker.AddAction(new WorkerTask
+            {
+                Action = () => Profile.ExportBuild(build, saveFileDialog.FileName),
+                Title = AppLocalization.GetLocalizedString("progress_dialog_title"),
+                Description = AppLocalization.GetLocalizedString("tab_presets_export")
+            });
+        }
+
+        private static void ExportAllBuilds()
+        {
+            FolderBrowserDialog folderBrowserDialog = new()
+            {
+                RootFolder = Environment.SpecialFolder.MyComputer,
+                ShowNewFolderButton = true
+            };
+            if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
+                return;
+            foreach (var build in Profile.WeaponBuilds)
+            {
+                App.Worker.AddAction(new WorkerTask
+                {
+                    Action = () => Profile.ExportBuild(build.Value, Path.Combine(folderBrowserDialog.SelectedPath, $"Weapon preset {build.Value.Name}.json")),
+                    Title = AppLocalization.GetLocalizedString("progress_dialog_title"),
+                    Description = AppLocalization.GetLocalizedString("tab_presets_export")
+                });
+            }
+        }
+
+        private static void ImportBuildsFromFiles()
+        {
+            OpenFileDialog openFileDialog = new()
+            {
+                Filter = "Файл JSON (*.json)|*.json|All files (*.*)|*.*",
+                RestoreDirectory = true,
+                Multiselect = true
+            };
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+            foreach (var file in openFileDialog.FileNames)
+            {
+                App.Worker.AddAction(new WorkerTask
+                {
+                    Action = () => Profile.ImportBuildFromFile(file),
+                    Title = AppLocalization.GetLocalizedString("progress_dialog_title"),
+                    Description = AppLocalization.GetLocalizedString("tab_presets_import")
+                });
+            }
+        }
+
+        private async Task RemoveBuildFromProfile(object obj)
+        {
+            if (!string.IsNullOrEmpty(obj?.ToString()) && await Dialogs.YesNoDialog(this, "remove_preset_dialog_title", "remove_preset_dialog_caption"))
+                Profile.RemoveBuild(obj.ToString());
+        }
+
+        private async Task RemoveAllBuildsFromProfile()
+        {
+            if (await Dialogs.YesNoDialog(this, "remove_preset_dialog_title", "remove_presets_dialog_caption"))
+                Profile.RemoveBuilds();
+        }
     }
 }
