2522ce9f272006d6ff3b3eb01920060012829e22
Dmitry Khudyakov <skit.92@mail.com>
2022-07-09 16:01:41 +0600

    WIP: LocalizationEditorViewModel

---
+++ SPT-AKI Profile Editor.Tests/ViewModelsTests/LocalizationEditorViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using SPT_AKI_Profile_Editor.Core;
+using SPT_AKI_Profile_Editor.Views;
+
+namespace SPT_AKI_Profile_Editor.Tests.ViewModelsTests
+{
+    internal class LocalizationEditorViewModelTests
+    {
+        [Test]
+        public void LocalizationEditorViewModelForEnInitializeCorrectly()
+        {
+            LocalizationEditorViewModel lEditor = TestViewModel(new AppLocalization("en"));
+            Assert.That(lEditor, Is.Not.Null);
+            Assert.That(lEditor.Localization, Is.Not.Null);
+            Assert.That(lEditor.CanSelectKey, Is.False);
+        }
+
+        [Test]
+        public void LocalizationEditorViewModelForNewInitializeCorrectly()
+        {
+            LocalizationEditorViewModel lEditor = TestViewModel();
+            Assert.That(lEditor, Is.Not.Null);
+            Assert.That(lEditor.Localization, Is.Not.Null);
+            Assert.That(lEditor.CanSelectKey, Is.True);
+            Assert.That(lEditor.AvailableKeys, Is.Not.Null);
+            Assert.That(lEditor.AvailableKeys.Count, Is.GreaterThanOrEqualTo(1));
+        }
+
+        private static LocalizationEditorViewModel TestViewModel(AppLocalization appLocalization = null) => new(appLocalization);
+    }
+}
---
+++ SPT-AKI Profile Editor/Views/LocalizationEditorViewModel.cs
@@ -0,0 +1,19 @@
+﻿using SPT_AKI_Profile_Editor.Core;
+using SPT_AKI_Profile_Editor.Helpers;
+using System.Collections.Generic;
+
+namespace SPT_AKI_Profile_Editor.Views
+{
+    public class LocalizationEditorViewModel : BindableViewModel
+    {
+        public AppLocalization Localization { get; set; }
+        public bool CanSelectKey { get; set; }
+        public Dictionary<string, string> AvailableKeys { get; set; }
+
+        public LocalizationEditorViewModel(AppLocalization appLocalization = null)
+        {
+            Localization = appLocalization ?? AppLocalization;
+            CanSelectKey = appLocalization == null;
+        }
+    }
+}
