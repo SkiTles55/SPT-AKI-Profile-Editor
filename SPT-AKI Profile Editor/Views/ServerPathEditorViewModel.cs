e22dc525f8b3d77cb71a5bfab11311f60d3828d1
Dmitry Khudyakov <skit.92@mail.com>
2022-07-31 23:07:24 +0600

    Added ServerPathEditorViewModel

---
+++ SPT-AKI Profile Editor/Views/ServerPathEditorViewModel.cs
@@ -0,0 +1,33 @@
+﻿using SPT_AKI_Profile_Editor.Helpers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SPT_AKI_Profile_Editor.Views
+{
+    public class ServerPathEditorViewModel : ClosableDialogViewModel
+    {
+        public ServerPathEditorViewModel(IEnumerable<(string key, string path, bool result)> paths,
+                                         RelayCommand retryCommand)
+        {
+            Paths = paths.Select(x => new ServerPathEntry(x));
+            RetryCommand = retryCommand;
+        }
+
+        public IEnumerable<ServerPathEntry> Paths { get; }
+        public RelayCommand RetryCommand { get; }
+
+        public class ServerPathEntry
+        {
+            public ServerPathEntry((string key, string path, bool result) resultEntry)
+            {
+                Key = resultEntry.key;
+                Path = resultEntry.path;
+                IsFounded = resultEntry.result;
+            }
+
+            private string Key { get; }
+            private string Path { get; set; }
+            private bool IsFounded { get; }
+        }
+    }
+}
