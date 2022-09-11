6cb8bcc95febe8c20f4f3bd292c857d15ede5ea5
Dmitry Khudyakov <skit.92@mail.com>
2022-09-11 19:06:51 +0600

    HasLevelIssue additional fix

--- SPT-AKI Profile Editor/Core/ProfileClasses/CharacterTraderStandingExtended.cs
+++ SPT-AKI Profile Editor/Core/ProfileClasses/CharacterTraderStandingExtended.cs
@@ -49,9 +49,8 @@ namespace SPT_AKI_Profile_Editor.Core.ProfileClasses
 
         public bool HasLevelIssue(int? level)
         {
-            var requeredLoyalityLevel = Math.Max(0, LoyaltyLevel - 1);
-            return TraderBase?.LoyaltyLevels?.Count > requeredLoyalityLevel
-            && TraderBase?.LoyaltyLevels[requeredLoyalityLevel].MinLevel > level;
+            var currentLoyalityLevel = Math.Min(Math.Max(0, LoyaltyLevel - 1), MaxLevel);
+            return TraderBase?.LoyaltyLevels[currentLoyalityLevel].MinLevel > level;
         }
 
         private void SetSalesSum(int value)
