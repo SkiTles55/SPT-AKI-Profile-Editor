d6d099105f3ea3a07ea10efd10adaf4d720a2bdb
Dmitry Khudyakov <skit.92@mail.com>
2022-12-21 22:32:26 +0600

    Quest names fix

--- SPT-AKI Profile Editor/Core/ProfileClasses/Profile.cs
+++ SPT-AKI Profile Editor/Core/ProfileClasses/Profile.cs
@@ -144,7 +144,7 @@ namespace SPT_AKI_Profile_Editor.Core.ProfileClasses
             {
                 quest.QuestQid = quest.Qid;
                 quest.Type = QuestType.Standart;
-                if (AppData.ServerDatabase.LocalesGlobal.ContainsKey(quest.Qid.Name()) || profile.Characters.Pmc.RepeatableQuests == null || profile.Characters.Pmc.RepeatableQuests.Length == 0)
+                if (AppData.ServerDatabase.LocalesGlobal.ContainsKey(quest.Qid.QuestName()) || profile.Characters.Pmc.RepeatableQuests == null || profile.Characters.Pmc.RepeatableQuests.Length == 0)
                 {
                     quest.QuestTrader = AppData.ServerDatabase.QuestsData.ContainsKey(quest.Qid) ? AppData.ServerDatabase.QuestsData[quest.Qid].TraderId : "unknown";
                     quest.QuestData = AppData.ServerDatabase.QuestsData.ContainsKey(quest.Qid) ? AppData.ServerDatabase.QuestsData[quest.Qid] : null;
