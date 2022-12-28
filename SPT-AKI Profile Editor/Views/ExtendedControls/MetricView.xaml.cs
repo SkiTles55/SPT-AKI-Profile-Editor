b4f01da06edd8206e369cd9940725cae94cb71d0
Dmitry Khudyakov <skit.92@mail.com>
2022-12-28 21:41:40 +0600

    Added MetricView

---
+++ SPT-AKI Profile Editor/Views/ExtendedControls/MetricView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using SPT_AKI_Profile_Editor.Core.ProfileClasses;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SPT_AKI_Profile_Editor.Views.ExtendedControls
+{
+    /// <summary>
+    /// Логика взаимодействия для MetricView.xaml
+    /// </summary>
+    public partial class MetricView : UserControl
+    {
+        public static readonly DependencyProperty CharacterMetricProperty =
+            DependencyProperty.Register(nameof(CharacterMetric), typeof(CharacterMetric), typeof(MetricView), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty MetricNameProperty =
+            DependencyProperty.Register(nameof(MetricName), typeof(string), typeof(MetricView), new PropertyMetadata(null));
+
+        public MetricView()
+        {
+            InitializeComponent();
+        }
+
+        public CharacterMetric CharacterMetric
+        {
+            get { return (CharacterMetric)GetValue(CharacterMetricProperty); }
+            set { SetValue(CharacterMetricProperty, value); }
+        }
+
+        public string MetricName
+        {
+            get { return (string)GetValue(MetricNameProperty); }
+            set { SetValue(MetricNameProperty, value); }
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
