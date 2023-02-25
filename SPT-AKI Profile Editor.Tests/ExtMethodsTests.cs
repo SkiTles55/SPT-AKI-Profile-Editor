1f74b58e351c6c7ee6478bb692e13dc1eaf663a6
Dmitry Khudyakov <skit.92@mail.com>
2023-02-25 17:03:16 +0600

    Additional test for id generator

--- SPT-AKI Profile Editor.Tests/ExtMethodsTests.cs
+++ SPT-AKI Profile Editor.Tests/ExtMethodsTests.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using SPT_AKI_Profile_Editor.Core;
+using SPT_AKI_Profile_Editor.Tests.Hepers;
+using System.Linq;
 
 namespace SPT_AKI_Profile_Editor.Tests
 {
@@ -7,5 +9,19 @@ namespace SPT_AKI_Profile_Editor.Tests
     {
         [Test]
         public void IdNotEmpty() => Assert.IsNotEmpty(ExtMethods.GenerateNewId(new string[] { "testid" }));
+
+        [Test]
+        public void CanGenerateUniqueId()
+        {
+            TestHelpers.LoadDatabaseAndProfile();
+            var idsArray = AppData.Profile.Characters.Pmc.Inventory.Items.Select(x => x.Id).ToList();
+            var startCount = idsArray.Count;
+
+            for (int i = 0; i < 150; i++)
+                idsArray.Add(ExtMethods.GenerateNewId(idsArray));
+
+            Assert.IsTrue(idsArray.Count == startCount + 150, "List od id's does't have new id's");
+            Assert.IsTrue(idsArray.Count == idsArray.Distinct().Count(), "List od id's have duplicates");
+        }
     }
 }
