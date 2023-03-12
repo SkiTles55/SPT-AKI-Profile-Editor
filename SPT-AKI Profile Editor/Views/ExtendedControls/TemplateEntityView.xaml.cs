6e38fbb7a84cd6c43f06c2fb020bf5ef33df7f67
Dmitry Khudyakov <skit.92@mail.com>
2023-03-12 16:40:51 +0600

    TemplateEntityView mockup

---
+++ SPT-AKI Profile Editor/Views/ExtendedControls/TemplateEntityView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace SPT_AKI_Profile_Editor.Views.ExtendedControls
+{
+    /// <summary>
+    /// Логика взаимодействия для TemplateEntityView.xaml
+    /// </summary>
+    public partial class TemplateEntityView : UserControl
+    {
+        public TemplateEntityView() => InitializeComponent();
+    }
+}
