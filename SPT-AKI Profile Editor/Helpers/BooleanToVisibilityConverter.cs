a0225dc444cd76a6749f63bf9cbe6b337e3b7ae4
Dmitry Khudyakov <skit.92@mail.com>
2023-03-18 18:19:40 +0600

    Custom BooleanToVisibilityConverter

---
+++ SPT-AKI Profile Editor/Helpers/BooleanToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SPT_AKI_Profile_Editor.Helpers
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public Visibility True { get; set; } = Visibility.Collapsed;
+        public Visibility False { get; set; } = Visibility.Visible;
+
+        public object Convert(object value,
+                              Type targetType,
+                              object parameter,
+                              CultureInfo culture) => value is bool boolean && boolean ? True : False;
+
+        public object ConvertBack(object value,
+                                  Type targetType,
+                                  object parameter,
+                                  CultureInfo culture) => throw new NotImplementedException();
+    }
+}
