49a5009f45eb8077c1dc1f84a6bbf4cf5fc0ca9d
Dmitry Khudyakov <skit.92@mail.com>
2023-07-16 20:09:32 +0600

    Added TextField with copy button

---
+++ SPT-AKI Profile Editor/Views/ExtendedControls/TextField.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SPT_AKI_Profile_Editor.Views.ExtendedControls
+{
+    /// <summary>
+    /// Логика взаимодействия для TextField.xaml
+    /// </summary>
+    public partial class TextField : UserControl
+    {
+        public TextField() => InitializeComponent();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is string text)
+                Clipboard.SetText(text);
+        }
+    }
+}
