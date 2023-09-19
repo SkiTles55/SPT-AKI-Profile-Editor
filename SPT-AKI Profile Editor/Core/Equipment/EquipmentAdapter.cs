7a947359a3e012a63328b791056651023484405b
Dmitry Khudyakov <skit.92@mail.com>
2023-09-19 22:47:22 +0600

    EquipmentAdapter refactoring

--- SPT-AKI Profile Editor/Core/Equipment/EquipmentAdapter.cs
+++ SPT-AKI Profile Editor/Core/Equipment/EquipmentAdapter.cs
@@ -13,146 +13,75 @@ namespace SPT_AKI_Profile_Editor.Core.Equipment
                                                                 Dictionary<string, string> localesGlobal,
                                                                 AppLocalization localization,
                                                                 AppSettings appSettings)
-        {
-            var emptySlotItemText = localization?.GetLocalizedString(emptySlotKey);
-            return new()
-            {
-                new EquipmentSlotItem(EarpieceSlotName(localesGlobal),
-                                      GetEquipment(appSettings.EarpieceSlotId, inventory.Items, equipmentSlot),
-                                      emptySlotItemText),
-                new EquipmentSlotItem(HeadwearSlotName(localesGlobal),
-                                      GetEquipment(appSettings.HeadwearSlotId, inventory.Items, equipmentSlot),
-                                      emptySlotItemText),
-                new EquipmentSlotItem(FaceCoverSlotName(localesGlobal),
-                                      GetEquipment(appSettings.FaceCoverSlotId, inventory.Items, equipmentSlot),
-                                      emptySlotItemText),
-                new EquipmentSlotItem(EyewearSlotName(localesGlobal),
-                                      GetEquipment(appSettings.EyewearSlotId, inventory.Items, equipmentSlot),
-                                      emptySlotItemText),
-                new EquipmentSlotItem(ArmbandSlotName(localesGlobal),
-                                      GetEquipment(appSettings.ArmBandSlotId, inventory.Items, equipmentSlot),
-                                      emptySlotItemText),
-                new EquipmentSlotItem(ArmorVestSlotName(localesGlobal),
-                                      GetEquipment(appSettings.ArmorVestSlotId, inventory.Items, equipmentSlot),
-                                      emptySlotItemText),
-                new EquipmentSlotItem(TacticalVestSlotName(localesGlobal),
-                                      GetEquipment(appSettings.TacticalVestSlotId, inventory.Items, equipmentSlot),
-                                      emptySlotItemText),
-                new EquipmentSlotItem(PrimaryWeaponFirstSlotName(localesGlobal),
-                                      GetEquipment(appSettings.FirstPrimaryWeaponSlotId, inventory.Items, equipmentSlot),
-                                      emptySlotItemText),
-                new EquipmentSlotItem(PrimaryWeaponSecondSlotName(localesGlobal),
-                                      GetEquipment(appSettings.SecondPrimaryWeaponSlotId, inventory.Items, equipmentSlot),
-                                      emptySlotItemText),
-                new EquipmentSlotItem(HolsterSlotName(localesGlobal),
-                                      GetEquipment(appSettings.HolsterSlotId, inventory.Items, equipmentSlot),
-                                      emptySlotItemText),
-                new EquipmentSlotItem(ScabbardSlotName(localesGlobal),
-                                      GetEquipment(appSettings.ScabbardSlotId, inventory.Items, equipmentSlot),
-                                      emptySlotItemText),
-                new EquipmentSlotPockets(PocketsSlotName(localesGlobal), inventory?.PocketsItems),
-                new EquipmentSlotItem(BackpackSlotName(localesGlobal),
-                                      GetEquipment(appSettings.BackpackSlotId, inventory.Items, equipmentSlot),
-                                      emptySlotItemText),
-                new EquipmentSlotItem(SecuredContainerSlotName(localesGlobal),
-                                      GetEquipment(appSettings.SecuredContainerSlotId, inventory.Items, equipmentSlot),
-                                      emptySlotItemText)
-            };
-        }
+            => GetEquipmentSlots(inventory.Items,
+                                 equipmentSlot,
+                                 localesGlobal,
+                                 localization,
+                                 appSettings);
 
         public static List<EquipmentSlot> GetEquipmentFromBuild(EquipmentBuild build,
                                                                 Dictionary<string, string> localesGlobal,
                                                                 AppLocalization localization,
                                                                 AppSettings appSettings)
+            => GetEquipmentSlots(build.BuildItems,
+                                 build.Root,
+                                 localesGlobal,
+                                 localization,
+                                 appSettings);
+
+        private static List<EquipmentSlot> GetEquipmentSlots(IEnumerable<InventoryItem> items,
+                                                             string root,
+                                                             Dictionary<string, string> localesGlobal,
+                                                             AppLocalization localization,
+                                                             AppSettings appSettings)
         {
             var emptySlotItemText = localization?.GetLocalizedString(emptySlotKey);
-            var pocketsId = build.BuildItems?.Where(x => x.IsPockets).FirstOrDefault()?.Id;
+            var pocketsId = items?.Where(x => x.IsPockets).FirstOrDefault()?.Id;
             return new()
             {
-                new EquipmentSlotItem(EarpieceSlotName(localesGlobal),
-                                      GetEquipment(appSettings.EarpieceSlotId, build.BuildItems, build.Root),
+                new EquipmentSlotItem(GetSlotName("Earpiece", localesGlobal),
+                                      GetEquipment(appSettings.EarpieceSlotId, items, root),
                                       emptySlotItemText),
-                new EquipmentSlotItem(HeadwearSlotName(localesGlobal),
-                                      GetEquipment(appSettings.HeadwearSlotId, build.BuildItems, build.Root),
+                new EquipmentSlotItem(GetSlotName("Headwear", localesGlobal),
+                                      GetEquipment(appSettings.HeadwearSlotId, items, root),
                                       emptySlotItemText),
-                new EquipmentSlotItem(FaceCoverSlotName(localesGlobal),
-                                      GetEquipment(appSettings.FaceCoverSlotId, build.BuildItems, build.Root),
+                new EquipmentSlotItem(GetSlotName("FaceCover", localesGlobal),
+                                      GetEquipment(appSettings.FaceCoverSlotId, items, root),
                                       emptySlotItemText),
-                new EquipmentSlotItem(EyewearSlotName(localesGlobal),
-                                      GetEquipment(appSettings.EyewearSlotId, build.BuildItems, build.Root),
+                new EquipmentSlotItem(GetSlotName("Eyewear", localesGlobal),
+                                      GetEquipment(appSettings.EyewearSlotId, items, root),
                                       emptySlotItemText),
-                new EquipmentSlotItem(ArmbandSlotName(localesGlobal),
-                                      GetEquipment(appSettings.ArmBandSlotId, build.BuildItems, build.Root),
+                new EquipmentSlotItem(GetSlotName("Armband", localesGlobal),
+                                      GetEquipment(appSettings.ArmBandSlotId, items, root),
                                       emptySlotItemText),
-                new EquipmentSlotItem(ArmorVestSlotName(localesGlobal),
-                                      GetEquipment(appSettings.ArmorVestSlotId, build.BuildItems, build.Root),
+                new EquipmentSlotItem(GetSlotName("ArmorVest", localesGlobal),
+                                      GetEquipment(appSettings.ArmorVestSlotId, items, root),
                                       emptySlotItemText),
-                new EquipmentSlotItem(TacticalVestSlotName(localesGlobal),
-                                      GetEquipment(appSettings.TacticalVestSlotId, build.BuildItems, build.Root),
+                new EquipmentSlotItem(GetSlotName("TacticalVest", localesGlobal),
+                                      GetEquipment(appSettings.TacticalVestSlotId, items, root),
                                       emptySlotItemText),
-                new EquipmentSlotItem(PrimaryWeaponFirstSlotName(localesGlobal),
-                                      GetEquipment(appSettings.FirstPrimaryWeaponSlotId, build.BuildItems, build.Root),
+                new EquipmentSlotItem(GetSlotName("PrimaryWeaponFirst", localesGlobal),
+                                      GetEquipment(appSettings.FirstPrimaryWeaponSlotId, items, root),
                                       emptySlotItemText),
-                new EquipmentSlotItem(PrimaryWeaponSecondSlotName(localesGlobal),
-                                      GetEquipment(appSettings.SecondPrimaryWeaponSlotId, build.BuildItems, build.Root),
+                new EquipmentSlotItem(GetSlotName("PrimaryWeaponSecond", localesGlobal),
+                                      GetEquipment(appSettings.SecondPrimaryWeaponSlotId, items, root),
                                       emptySlotItemText),
-                new EquipmentSlotItem(HolsterSlotName(localesGlobal),
-                                      GetEquipment(appSettings.HolsterSlotId, build.BuildItems, build.Root),
+                new EquipmentSlotItem(GetSlotName("Holster", localesGlobal),
+                                      GetEquipment(appSettings.HolsterSlotId, items, root),
                                       emptySlotItemText),
-                new EquipmentSlotItem(ScabbardSlotName(localesGlobal),
-                                      GetEquipment(appSettings.ScabbardSlotId, build.BuildItems, build.Root),
+                new EquipmentSlotItem(GetSlotName("Scabbard", localesGlobal),
+                                      GetEquipment(appSettings.ScabbardSlotId, items, root),
                                       emptySlotItemText),
-                new EquipmentSlotPockets(PocketsSlotName(localesGlobal), build.BuildItems.Where(x => x.ParentId == pocketsId)),
-                new EquipmentSlotItem(BackpackSlotName(localesGlobal),
-                                      GetEquipment(appSettings.BackpackSlotId, build.BuildItems, build.Root),
+                new EquipmentSlotPockets(GetSlotName("Pockets", localesGlobal),
+                                         items.Where(x => x.ParentId == pocketsId)),
+                new EquipmentSlotItem(GetSlotName("Backpack", localesGlobal),
+                                      GetEquipment(appSettings.BackpackSlotId, items, root),
                                       emptySlotItemText),
-                new EquipmentSlotItem(SecuredContainerSlotName(localesGlobal),
-                                      GetEquipment(appSettings.SecuredContainerSlotId, build.BuildItems, build.Root),
+                new EquipmentSlotItem(GetSlotName("SecuredContainer", localesGlobal),
+                                      GetEquipment(appSettings.SecuredContainerSlotId, items, root),
                                       emptySlotItemText)
             };
         }
-
-        private static string EarpieceSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("Earpiece", localesGlobal);
-
-        private static string HeadwearSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("Headwear", localesGlobal);
-
-        private static string FaceCoverSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("FaceCover", localesGlobal);
-
-        private static string EyewearSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("Eyewear", localesGlobal);
-
-        private static string ArmbandSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("Armband", localesGlobal);
-
-        private static string ArmorVestSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("ArmorVest", localesGlobal);
-
-        private static string TacticalVestSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("TacticalVest", localesGlobal);
-
-        private static string PrimaryWeaponFirstSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("PrimaryWeaponFirst", localesGlobal);
-
-        private static string PrimaryWeaponSecondSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("PrimaryWeaponSecond", localesGlobal);
-
-        private static string HolsterSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("Holster", localesGlobal);
-
-        private static string ScabbardSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("Scabbard", localesGlobal);
-
-        private static string PocketsSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("Pockets", localesGlobal);
-
-        private static string BackpackSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("Backpack", localesGlobal);
-
-        private static string SecuredContainerSlotName(Dictionary<string, string> localesGlobal)
-            => GetSlotName("SecuredContainer", localesGlobal);
 
         private static string GetSlotName(string key, Dictionary<string, string> localesGlobal)
             => localesGlobal?.ContainsKey(key) == true ? localesGlobal[key] : key;
