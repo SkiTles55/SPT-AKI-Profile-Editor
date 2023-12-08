3201dacf1bac307d80be78364ba131612ee5a1b3
Dmitry Khudyakov <skit.92@mail.com>
2023-12-08 23:55:16 +0600

    Started work on transfer progress UI

---
+++ SPT-AKI Profile Editor/Views/ExtendedControls/InfoTransferSettings.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SPT_AKI_Profile_Editor.Views.ExtendedControls
+{
+    /// <summary>
+    /// Логика взаимодействия для InfoTransferSettings.xaml
+    /// </summary>
+    public partial class InfoTransferSettings : UserControl
+    {
+        public InfoTransferSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
