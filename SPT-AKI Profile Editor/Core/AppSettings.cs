fd39542a9145ad44114b731ca6b520f5280f544f
Dmitry Khudyakov <skit.92@mail.com>
2025-11-23 14:42:23 +0600

    change url to path

--- SPT-AKI Profile Editor/Core/AppSettings.cs
+++ SPT-AKI Profile Editor/Core/AppSettings.cs
@@ -30,7 +30,7 @@ namespace SPT_AKI_Profile_Editor.Core
         public readonly string sptProjectUrl = "https://www.sp-tarkov.com/";
 
         [JsonIgnore]
-        public readonly string modHelperUpdateUrl = "https://raw.githubusercontent.com/SkiTles55/SPT-AKI-Profile-Editor/master/SPT-AKI%20Profile%20Editor/ModHelper/";
+        public readonly string modHelperUpdateUrl = "https://raw.githubusercontent.com/SkiTles55/SPT-AKI-Profile-Editor/master/SPT-AKI%20Profile%20Editor.ModHelper/bin/Debug/net9.0/";
 
         [JsonIgnore]
         public readonly string modHelperHowToUseUrl = "https://github.com/SkiTles55/SPT-AKI-Profile-Editor/blob/master/Guidelines/ModHelper";
